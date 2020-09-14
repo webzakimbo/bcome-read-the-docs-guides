b6c0816d8fddb040e2e51ddb8a3284cab143bef4
webzakimbo <guillaume@webzakimbo.com>
2020-09-14 11:22:30 +0100

    Camel case title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'Bcome guides'
+project = u'Bcome Guides'
 copyright = u'2020 Webzakimbo Ltd.'
 
 author = u'Webzakimbo Ltd.'
